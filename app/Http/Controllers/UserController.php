e57776a6d0b36c35fd1353ed08c78e83c9142b58
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-10 13:05:46 +0300

    Created addFavorites Function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-  public function addFavorites(){
+  public function addFavorites(Request $request){
+    if (Auth::check()){
+
+      $fav = new Favorite();
+      $fav->user_id = $request->user_id;
+      $fav->item_id = $request->item_id;
+    
+      
+      $fav->save();
+      
+      return response()->json([
+          "status" => "Added to Favorites",
+          "favorites" => $fav
+      ], 200);
+      
+    }
+
+    return response()->json([
+      "status" => "You are not logged in"
+     
+  ], 200);
+  
+}
+
+
+
       
   }
-}
+
