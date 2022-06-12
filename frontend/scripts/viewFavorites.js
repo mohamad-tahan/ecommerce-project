3552714cf49c02aebcf07f61af027a64a4078654
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-13 00:02:20 +0300

    Added viewFavorites function

---
+++ frontend/scripts/viewFavorites.js
@@ -0,0 +1,26 @@
+function viewFavorites() {
+  axios({
+    method: "GET",
+    url: "http://127.0.0.1:8000/api/v1/admin/favorites",
+    headers : {
+        "Authorization": "Bearer " + localStorage.getItem("token")
+    }
+  }).then(function (response) {
+      console.log(response);
+    for (let i = 0; i < response.data.Categories.length; i++) {
+      var view = document.querySelector("#tab");
+      var viewClone = view.cloneNode(true);
+
+      viewClone.id = "viewss";
+
+      viewClone.querySelector("#id").innerText = response.data.Categories[i]["id"];
+      viewClone.querySelector("#user_id").innerText = response.data.Categories[i]["user_id"];
+
+      viewClone.querySelector("#item_id").innerText = response.data.Categories[i]["item_id"];
+
+      document.body.appendChild(viewClone);
+    }
+    document.querySelector("#view").remove;
+  });
+}
+viewFavorites();
